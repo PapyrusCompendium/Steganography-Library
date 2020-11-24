170c04ca7f30266e46eb1a2324ee77f0967b1b4b
Phillip H. Blanton <phillip@radwarrior.com>
2020-11-24 16:41:59 -0700

    Test image and debugging

--- Steganography-Library/Steganography-Library/SteganographicImage.cs
+++ Steganography-Library/Steganography-Library/SteganographicImage.cs
@@ -5,122 +5,123 @@ using System.Linq;
 
 namespace Steganography_Library
 {
-    public class SteganographicImage
-    {
-        public Bitmap Stegnograph { get; private set; }
-        public SteganographicImage(Bitmap image) =>
-            Stegnograph = image;
+	public class SteganographicImage
+	{
+		public Bitmap Stegnograph { get; private set; }
+		public SteganographicImage(Bitmap image) =>
+			 Stegnograph = image;
 
-        public bool FitsIntoImage(byte[] data) =>
-            data.Length <= TotalCapacity;
+		public bool FitsIntoImage(byte[] data) =>
+			 data.Length <= TotalCapacity;
 
-        public int TotalCapacity =>
-            ((Stegnograph.Width * Stegnograph.Height) - 8) / 2;
+		public int TotalCapacity =>
+			 ((Stegnograph.Width * Stegnograph.Height) - 8) / 2;
 
-        private bool GetLSB(byte value) =>
-            (value & 1) != 0;
+		private bool GetLSB(byte value) =>
+			 (value & 1) != 0;
 
-        private byte[] GetColourBytes(Color colour) =>
-            new byte[4] { colour.A, colour.R, colour.G, colour.B };
+		private byte[] GetColourBytes(Color colour) =>
+			 new byte[4] { colour.A, colour.R, colour.G, colour.B };
 
-        private byte SetLSB(bool bit, byte value)
-        {
-            if (GetLSB(value) && !bit)
-                return (byte)(value ^ 1); //Will flip the LSB to 0 if it is 1
-            else if (!GetLSB(value) && bit)
-                return (byte)(value | 1); //Will flip the LSB to 1 if it is 0
-            return value;
-        }
+		private byte SetLSB(bool bit, byte value)
+		{
+			if (GetLSB(value) && !bit)
+				return (byte)(value ^ 1); //Will flip the LSB to 0 if it is 1
+			else if (!GetLSB(value) && bit)
+				return (byte)(value | 1); //Will flip the LSB to 1 if it is 0
+			return value;
+		}
 
-        private byte[] BitArrayToByteArray(BitArray bits)
-        {
-            byte[] ret = new byte[(bits.Length - 1) / 8 + 1];
-            bits.CopyTo(ret, 0);
-            return ret;
-        }
+		private byte[] BitArrayToByteArray(BitArray bits)
+		{
+			byte[] ret = new byte[(bits.Length - 1) / 8 + 1];
+			bits.CopyTo(ret, 0);
+			return ret;
+		}
 
-        public byte[] DecodeImage(bool encrypted = false, string key = "")
-        {
-            bool[] bits = new bool[32];
+		public byte[] DecodeImage(bool encrypted = false, string key = "")
+		{
+			bool[] bits = new bool[32];
 
-            //We only want to read 8 pixels to decode the first 4 bytes, thats contains the length of the total data.
-            for (int x = 0; x < Math.Ceiling(8d / Stegnograph.Width); x++)
-                for (int y = 0; y < (Stegnograph.Height > 8 ? 8 : Stegnograph.Height); y++)
-                {
-                    byte[] colourBytes = GetColourBytes(Stegnograph.GetPixel(x, y));
+			//We only want to read 8 pixels to decode the first 4 bytes, thats contains the length of the total data.
+			for (int x = 0; x < Math.Ceiling(8d / Stegnograph.Width); x++)
+				for (int y = 0; y < (Stegnograph.Height > 8 ? 8 : Stegnograph.Height); y++)
+				{
+					byte[] colourBytes = GetColourBytes(Stegnograph.GetPixel(x, y));
 
-                    for (int i = 0; i < 4; i++)
-                        bits[(((x * Stegnograph.Width) + y) * 4) + i] = GetLSB(colourBytes[i]);
-                }
+					for (int i = 0; i < 4; i++)
+						bits[(((x * Stegnograph.Width) + y) * 4) + i] = GetLSB(colourBytes[i]);
+				}
 
-            byte[] lengthBytes = BitArrayToByteArray(new BitArray(bits));
-            int length = BitConverter.ToInt32(lengthBytes, 0);
+			byte[] lengthBytes = BitArrayToByteArray(new BitArray(bits));
+			int length = BitConverter.ToInt32(lengthBytes, 0);
 
-            if (encrypted)
-                return Cryptography.Decrypt(DecodeImageLSB(length), key);
-            else
-                return DecodeImageLSB(length);
-        }
+			if (encrypted)
+				return Cryptography.Decrypt(DecodeImageLSB(length), key);
+			else
+				return DecodeImageLSB(length);
+		}
 
-        private byte[] DecodeImageLSB(int length)
-        {
-            int totalPixelsEncoded = length * 2;
-            bool[] bits = new bool[length * 8];
+		private byte[] DecodeImageLSB(int length)
+		{
+			int totalPixelsEncoded = length * 2;
+			bool[] bits = new bool[length * 8];
 
-            for (int x = 0; x < Stegnograph.Width; x++)
-                for (int y = 0; y < Stegnograph.Height; y++)
-                {
-                    if ((x * Stegnograph.Width) + y < 8)
-                        continue;
+			for (int x = 0; x < Stegnograph.Width; x++)
+				for (int y = 0; y < Stegnograph.Height; y++)
+				{
+					if ((x * Stegnograph.Width) + y < 8)
+						continue;
 
-                    if ((x * Stegnograph.Width) + y - 8 >= totalPixelsEncoded)
-                        return BitArrayToByteArray(new BitArray(bits.ToArray()));
+					if ((x * Stegnograph.Width) + y - 8 >= totalPixelsEncoded)
+						return BitArrayToByteArray(new BitArray(bits.ToArray()));
 
-                    byte[] colourBytes = GetColourBytes(Stegnograph.GetPixel(x, y));
+					byte[] colourBytes = GetColourBytes(Stegnograph.GetPixel(x, y));
 
-                    for (int i = 0; i < 4; i++)
-                        bits[(((x * Stegnograph.Width) + y) * 4) + i - 32] = GetLSB(colourBytes[i]);
-                }
+					for (int i = 0; i < 4; i++)
+						bits[(((x * Stegnograph.Width) + y) * 4) + i - 32] = GetLSB(colourBytes[i]);
+				}
 
-            return BitArrayToByteArray(new BitArray(bits.ToArray()));
-        }
+			return BitArrayToByteArray(new BitArray(bits.ToArray()));
+		}
 
-        public Bitmap EncodeImage(byte[] data, bool encrypted = false, string key = "")
-        {
-            if (encrypted)
-                data = Cryptography.Encrypt(data, key);
+		public Bitmap EncodeImage(byte[] data, bool encrypted = false, string key = "")
+		{
+			if (encrypted)
+				data = Cryptography.Encrypt(data, key);
 
-            byte[] formattedData = new byte[data.Length + 4];
-            BitConverter.GetBytes((Int32)data.Length).CopyTo(formattedData, 0);
-            data.CopyTo(formattedData, 4);
+			byte[] formattedData = new byte[data.Length + 4];
+			byte[] bytelength = BitConverter.GetBytes(data.Length);
+			bytelength.CopyTo(formattedData, 0);
+			data.CopyTo(formattedData, 4);
 
-            Stegnograph = EncodeImageLSB(formattedData);
-            return Stegnograph;
-        }
+			Stegnograph = EncodeImageLSB(formattedData);
+			return Stegnograph;
+		}
 
-        private Bitmap EncodeImageLSB(byte[] data)
-        {
-            Bitmap bitMap = new Bitmap(Stegnograph.Width, Stegnograph.Height);
+		private Bitmap EncodeImageLSB(byte[] data)
+		{
+			Bitmap bitMap = new Bitmap(Stegnograph.Width, Stegnograph.Height);
 
-            using (Graphics graphics = Graphics.FromImage(bitMap))
-                graphics.DrawImage(Stegnograph, 0, 0);
+			using (Graphics graphics = Graphics.FromImage(bitMap))
+				graphics.DrawImage(Stegnograph, 0, 0);
 
-            BitArray bits = new BitArray(data);
+			BitArray bits = new BitArray(data);
 
-            for (int x = 0; x < Stegnograph.Width; x++)
-                for (int y = 0; y < Stegnograph.Height; y++)
-                {
-                    if ((((x * Stegnograph.Width) + y) * 4) >= bits.Length)
-                        return bitMap;
+			for (int x = 0; x < Stegnograph.Width; x++)
+				for (int y = 0; y < Stegnograph.Height; y++)
+				{
+					if ((((x * Stegnograph.Width) + y) * 4) >= bits.Length)
+						return bitMap;
 
-                    byte[] colourBytes = GetColourBytes(bitMap.GetPixel(x, y));
-                    for (int i = 0; i < 4; i++)
-                        colourBytes[i] = SetLSB(bits[(((x * Stegnograph.Width) + y) * 4) + i], colourBytes[i]);
+					byte[] colourBytes = GetColourBytes(bitMap.GetPixel(x, y));
+					for (int i = 0; i < 4; i++)
+						colourBytes[i] = SetLSB(bits[(((x * Stegnograph.Width) + y) * 4) + i], colourBytes[i]);
 
-                    bitMap.SetPixel(x, y, Color.FromArgb(colourBytes[0], colourBytes[1], colourBytes[2], colourBytes[3]));
-                }
+					bitMap.SetPixel(x, y, Color.FromArgb(colourBytes[0], colourBytes[1], colourBytes[2], colourBytes[3]));
+				}
 
-            return bitMap;
-        }
-    }
+			return bitMap;
+		}
+	}
 }
--- stegolibtest/stegolibtest/Program.cs
+++ stegolibtest/stegolibtest/Program.cs
@@ -5,17 +5,19 @@ using System.Text;
 
 namespace stegolibtest
 {
-    class Program
-    {
-        static void Main()
-        {
-            //Must be SAVED as a png, can be loaded FROM any format!
-            SteganographicImage stegoImage = new SteganographicImage(new Bitmap(Image.FromFile("Image.png")));
-            stegoImage.EncodeImage(Encoding.UTF8.GetBytes("Secret Message")).Save("NewImage.png");
+	class Program
+	{
+		static void Main()
+		{
+			//Must be SAVED as a png, can be loaded FROM any format!
+			SteganographicImage stegoImage = new SteganographicImage(new Bitmap(Image.FromFile("TestImages/Image.png")));
+			var encodedimage = stegoImage.EncodeImage(Encoding.UTF8.GetBytes("Secret Message"));
+			encodedimage.Save("TestImages/NewImage.png");
 
-            //Decoding, load from png
-            stegoImage = new SteganographicImage(new Bitmap(Image.FromFile("NewImage.png")));
-            Console.WriteLine(stegoImage.DecodeImage());
-        }
-    }
+			//Decoding, load from png
+			stegoImage = new SteganographicImage(new Bitmap(Image.FromFile("TestImages/NewImage.png")));
+			string decoded = Encoding.UTF8.GetString(stegoImage.DecodeImage());
+			Console.WriteLine(decoded);
+		}
+	}
 }
