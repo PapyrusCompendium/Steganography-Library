cdfc4d75b146c7d562dd6621bad91bdebb48e4d4
PapyrusCompendium <51677630+PapyrusCompendium@users.noreply.github.com>
2022-02-22 23:58:46 -0500

    Added unit test for encryption.

--- Steganography-Library/SteganographyLibrary.Tests/SteganographicImageTests.cs
+++ Steganography-Library/SteganographyLibrary.Tests/SteganographicImageTests.cs
@@ -5,6 +5,7 @@ using Xunit;
 
 namespace SteganographyLibrary.Tests {
     public class SteganographicImageTests {
+        private const string TEST_PASSWORD = "Password";
 
         [Fact]
         public void Does_Work_Both_Ways_No_Encryption() {
@@ -15,6 +16,19 @@ namespace SteganographyLibrary.Tests {
 
             stegoImage = new SteganographicImage(encodedImage);
             var encodedData = stegoImage.GetEncodedDataFromBitmap();
+
+            Assert.Equal(encodedMessage, Encoding.UTF8.GetString(encodedData));
+        }
+
+        [Fact]
+        public void Does_Work_Both_Ways_With_Encryption() {
+            var encodedMessage = "This is an encoded message";
+
+            var stegoImage = new SteganographicImage(new Bitmap(Image.FromFile("TestImage.png")));
+            var encodedImage = stegoImage.EncodeDataInBitmap(Encoding.UTF8.GetBytes(encodedMessage), TEST_PASSWORD);
+
+            stegoImage = new SteganographicImage(encodedImage);
+            var encodedData = stegoImage.GetEncodedDataFromBitmap(TEST_PASSWORD);
 
             Assert.Equal(encodedMessage, Encoding.UTF8.GetString(encodedData));
         }
